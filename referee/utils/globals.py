62534c4ea8bc03e7722f309136a81f8618780aea
aayguade <arnau.ayguade01@estudiant.upf.edu>
2026-01-16 13:25:19 +0100

    Add missing files

--- referee/utils/globals.py
+++ referee/utils/globals.py
@@ -3,9 +3,5 @@ DERBERTA_DIR = "pretrained/deberta"
 CHECKPOINTS_DIR = "pretrained/checkpoints"
 
 
-<<<<<<< HEAD
 DATA_DIR = "../data/CAT_processed.json"
 DATA_DIR_ENGLISH = "../data/CAT_processed_translated.json"
-=======
-DATA_DIR = "../data/CAT_processed.json"
->>>>>>> f64d09b27b422c05b55fbba9df02f464027f50dc
