68739a9a4ac7f8f0eddcc91df4989755f0b9b859
aayguade <arnau.ayguade01@estudiant.upf.edu>
2026-03-24 15:56:04 +0100

    Correct catalan version of ASSET

--- dataset/install_dataset.py
+++ dataset/install_dataset.py
@@ -2,23 +2,37 @@ from datasets import load_dataset
 import json
 
 # Load dataset
-ds = load_dataset("facebook/asset", "simplification")
+ds = load_dataset("facebook/asset", "ratings")
 
-dataset = ds["validation"]
+dataset = ds["full"]
 
 new_data = []
 
+count = 0
+
 for example in dataset:
     original = example["original"]
-    simplifications = example["simplifications"]
+    simplification = example["simplification"]
+    rating = example["rating"]
     
-    for simpl in simplifications:
-        new_data.append({
-            "original": original,
-            "simplification": simpl
-        })
+    # for simpl in simplifications:
+    #    new_data.append({
+    #         "original": original,
+    #         "simplification": simpl
+    #     })
+    if rating > 30:
+        label = 1
+        count += 1
+    else:
+        label = 0
 
-with open("../data/asset.json", "w", encoding="utf-8") as f:
+    new_data.append({
+        "original": original,
+        "simplification": simplification,
+        "label": label 
+    })
+
+with open("../data/asset_ratings.json", "w", encoding="utf-8") as f:
     json.dump(new_data, f, ensure_ascii=False, indent=2)
 
-print(f"Saved {len(new_data)} samples.")
+print(f" {count} out of {len(new_data)} are accepted")
